bcd6bdddbb99486cb48ea7c5b906e9198ae9efcb
Jarred Stone <Jarred.Stone37@gmail.com>
2021-04-16 09:07:07 -0400

    About Me Information

--- hello.py
+++ hello.py
@@ -1,3 +1,16 @@
-shrimp = "Hello world"
+first_name = "Jarred"
+last_name = "Stone"
+age = "27"
 
-print(shrimp)
+my_favorite_things = ["netflix", "anime", "sports", ["call of duty", "FIFA", "Assassin's Creed"], ["wings", "oriental cuisine", "breakfast"]]
+
+my_favorite_videogames = my_favorite_things[3]
+
+my_favorite_foods = my_favorite_things[4]
+
+print(first_name, last_name)
+print(age)
+print("my favorite ways to waste time", my_favorite_things[0:3])
+print("my favorite video games", my_favorite_videogames)
+print("My favorite foods are", my_favorite_foods)
+
